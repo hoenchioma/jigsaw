f74608a4536e111bdf7c3dae7846b500959fe199
farhankabir12 <43378296+farhankabir12@users.noreply.github.com>
2019-04-26 00:46:06 +0600

    Fixed calendar population issue

--- src/com/jigsaw/gui/CalendarViewController.java
+++ src/com/jigsaw/gui/CalendarViewController.java
@@ -1,6 +1,6 @@
 package com.jigsaw.gui;
 
-import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import java.net.URL;
 import java.lang.String;
@@ -16,10 +16,9 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.layout.AnchorPane;
-import com.jfoenix.controls.JFXTreeTableView;
-import com.jfoenix.controls.RecursiveTreeItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -27,49 +26,45 @@ import javafx.scene.control.TreeItem;
 import javafx.util.Callback;
 
 
-public class CalendarViewController implements Initializable {
+public class CalendarViewController{
 
-    // FIXME: This scene doesn't load
+    @FXML
+    private AnchorPane anchorPaneID;
 
-    @FXML private DatePicker datePicker;
-    @FXML private AnchorPane anchorPaneID;
-    @FXML private JFXTreeTableView<CalendarEntry> treeView;
+    @FXML
+    private JFXDatePicker datePickerID;
+
+    @FXML
+    private JFXButton openProjectButtonID;
+
+    @FXML
+    private Label projectLabelID;
+
+    @FXML
+    private JFXTreeTableView<CalendarEntry> treeView;
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        ////////////
-        JFXTreeTableColumn<CalendarEntry, String> taskName = new JFXTreeTableColumn<>("Task Name");
-        taskName.setPrefWidth(150);
-        // taskName.setCellValueFactory(param -> param.getValue().getValue().taskName);
-        taskName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<CalendarEntry, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(JFXTreeTableColumn.CellDataFeatures<CalendarEntry, String> param) {
-                return param.getValue().getValue().taskName;
-            }
-        });
+    @FXML
+    private void filter(ActionEvent event) {
+    }
 
-        log("mew");
 
-        JFXTreeTableColumn<CalendarEntry, String> description = new JFXTreeTableColumn<>("Description");
-        taskName.setPrefWidth(150);
-        // taskName.setCellValueFactory(param -> param.getValue().getValue().description);
-        taskName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<CalendarEntry, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<CalendarEntry, String> param) {
-                return param.getValue().getValue().description;
-            }
-        });
 
-        JFXTreeTableColumn<CalendarEntry, String> members = new JFXTreeTableColumn<>("Members");
-        taskName.setPrefWidth(150);
-        //taskName.setCellValueFactory(param -> param.getValue().getValue().members);
-        taskName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<CalendarEntry, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<CalendarEntry, String> param) {
-                return param.getValue().getValue().members;
-            }
-        });
+    @FXML
+    void DatePickerAction(ActionEvent event) {
+
+        JFXTreeTableColumn<CalendarEntry, String> taskNameCol = new JFXTreeTableColumn<>("Task Name");
+        taskNameCol.setPrefWidth(150);
+        taskNameCol.setCellValueFactory(param -> param.getValue().getValue().taskName);
+
+        JFXTreeTableColumn<CalendarEntry, String> descriptionCol = new JFXTreeTableColumn<>("Task Description");
+        descriptionCol.setPrefWidth(150);
+        descriptionCol.setCellValueFactory(param -> param.getValue().getValue().description);
+
+        JFXTreeTableColumn<CalendarEntry,String> memberCol = new JFXTreeTableColumn<>("Related Members");
+        memberCol.setPrefWidth(150);
+        memberCol.setCellValueFactory(param -> param.getValue().getValue().members);
+
 
         ArrayList<String> assigneesList = new ArrayList<String>();
         assigneesList.add("MemberList1");
@@ -83,55 +78,39 @@ public class CalendarViewController implements Initializable {
         taskList.add(new ProjectTask("Wadith", LocalDateTime.now(), "User 5","001",assigneesList));
         taskList.add(new ProjectTask("Shamim", LocalDateTime.now(), "User 6","001",assigneesList));
 
-        ObservableList<CalendarEntry> tasks = FXCollections.observableArrayList();
-        for(int i = 0; i < taskList.size(); i++){
-            if(taskList.get(i).getDeadline().toLocalDate().equals(datePicker.getValue())) {
-                //tasks.add(new CalendarEntry(taskList.get(i)));
-               /* String name = taskList.get(i).getName();
+
+        ObservableList<CalendarEntry> taskEntry = FXCollections.observableArrayList();
+        for(int i  = 0; i<taskList.size(); i++){
+            if(taskList.get(i).getDeadline().toLocalDate().equals(datePickerID.getValue())){
+                String name = taskList.get(i).getName();
                 String des = taskList.get(i).getDetails();
-                ArrayList<User> memberList = taskList.get(i).getAssignees();
-                StringBuilder memberName = new StringBuilder(memberList.get(0).getUsername());
-                for(int j = 1; i<memberList.size(); j++){
-                    memberName.append(" ").append(memberList.get(j).getUsername());
+                StringBuilder memberName = new StringBuilder(assigneesList.get(0));
+                for(int j = 1; j<assigneesList.size(); j++){
+                    memberName.append(" ").append(assigneesList.get(j));
                 }
-                tasks.add(new CalendarEntry(name, des, memberName.toString()));*/
+                taskEntry.add(new CalendarEntry(name, des, memberName.toString()));
             }
         }
 
-
-        final TreeItem<CalendarEntry> root = new RecursiveTreeItem<CalendarEntry>(tasks, RecursiveTreeObject::getChildren);
-        log(Boolean.toString(treeView == null));
-        treeView.getColumns().setAll(taskName, members, description);
+        final TreeItem<CalendarEntry> root = new RecursiveTreeItem<CalendarEntry>(taskEntry, RecursiveTreeObject::getChildren);
+        treeView.getColumns().setAll(taskNameCol, descriptionCol, memberCol);
         treeView.setRoot(root);
         treeView.setShowRoot(false);
-    }
 
-    @FXML
-    private void filter(ActionEvent event) {
+
     }
 
     class CalendarEntry extends RecursiveTreeObject<CalendarEntry> {
+
         StringProperty taskName;
-        StringProperty members;
         StringProperty description;
+        StringProperty members;
 
         public CalendarEntry(String taskName, String description, String members) {
             this.taskName = new SimpleStringProperty(taskName);
             this.description = new SimpleStringProperty(description);
             this.members = new SimpleStringProperty(members);
-            /*ArrayList<User> memberList = task.getAssignees();
-            StringBuilder memberName = new StringBuilder();
-            memberName = new StringBuilder(memberList.get(0).getUsername());
-            for(int i = 1; i<memberList.size(); i++){
-                memberName.append(" ").append(memberList.get(i).getUsername());
-            }
-
-            this.members = new SimpleStringProperty(memberName.toString());*/
         }
 
-    }
-
-    private void log(String str) {
-        System.out.println(this.getClass().getCanonicalName() + ": " + str);
     }
 }
