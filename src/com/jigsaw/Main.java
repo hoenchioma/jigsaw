5b9fa84931b0c30b32fb6b07405551074fa801ce
Raheeb Hassan <raheeb998@gmail.com>
2019-04-30 10:31:22 +0600

    Deleted redundant files, minor edits

--- src/com/jigsaw/Main.java
+++ src/com/jigsaw/Main.java
@@ -1,16 +1,19 @@
 package com.jigsaw;
 
+import com.jigsaw.gui.login.LoginViewController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * This is the main point of access of the application
+ */
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui/login/LoginView.fxml"));
+        Parent root = LoginViewController.getRoot();
         primaryStage.setTitle("Jigsaw");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
