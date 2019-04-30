9b313438ada7409b1c39f13128c2e00ae24c9e2e
Raheeb Hassan <raheeb998@gmail.com>
2019-04-30 10:22:46 +0600

    Fixed chat view resizability issue, update project view scene loading

--- src/com/jigsaw/gui/main/ProjectViewController.java
+++ src/com/jigsaw/gui/main/ProjectViewController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -143,6 +144,10 @@ public class ProjectViewController implements Initializable {
      */
     private void loadUI(Pane root) {
         UIPane.getChildren().setAll(root);
+        AnchorPane.setTopAnchor(root, 0.0);
+        AnchorPane.setBottomAnchor(root, 0.0);
+        AnchorPane.setLeftAnchor(root, 0.0);
+        AnchorPane.setRightAnchor(root, 0.0);
     }
 
     /**
@@ -167,9 +172,10 @@ public class ProjectViewController implements Initializable {
      */
     private void changeScene(Pane sceneView, ActionEvent event, boolean resizability) throws IOException {
         Scene scene = new Scene(sceneView);
-        Stage window = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.setResizable(resizability);
         window.show();
+        window.centerOnScreen();
     }
 }
