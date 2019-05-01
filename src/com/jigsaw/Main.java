c829063a61f4b346a9457a83ef36e6eb2bdd2f5e
Raheeb Hassan <raheeb998@gmail.com>
2019-05-01 09:36:52 +0600

    Added application icon

--- src/com/jigsaw/Main.java
+++ src/com/jigsaw/Main.java
@@ -4,6 +4,7 @@ import com.jigsaw.gui.login.LoginViewController;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +17,7 @@ public class Main extends Application {
         Parent root = LoginViewController.getRoot();
         primaryStage.setTitle("Jigsaw");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("/com/jigsaw/gui/resources/JIGSAW-ICON.png"));
         primaryStage.show();
         primaryStage.setResizable(false);
     }
